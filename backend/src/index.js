e345c425b16fddc9cba8a78902e02a5dfe418212
skd19092003 <skd19092003@gmail.com>
2025-11-17 14:02:43 +0530

    port

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
 const __dirname = path.resolve();
 
